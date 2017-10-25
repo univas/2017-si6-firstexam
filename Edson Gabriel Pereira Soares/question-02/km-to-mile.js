ca456ca64402aa47fbf5e5834eadb471acfd222e
EGGabriel <ps.eggabriel@gmail.com>
2017-10-25 20:10:10 -0200

    Add First Exame

---
+++ Edson Gabriel Pereira Soares/question-02/km-to-mile.js
@@ -0,0 +1,4 @@
+let x = process.argv[2]
+
+x = (x * 0.62137119) 
+console.log(x.toFixed(2))
---
+++ Edson Gabriel Pereira Soares/question-03/check-student.js
@@ -0,0 +1,25 @@
+var students = require('./studentsDB.json')
+var x = process.argv[2]
+var situacao
+
+for (var i = 0; i < students.length; i++) {
+    var student = students[i];
+    if (x==student.id) {
+        var media = 0
+        for (var z = 0; z < student.grades.length; z++) {
+            var aux = student.grades[z];
+            media+=aux
+        }
+        console.log(media.toFixed(2)/4)
+        if (media/4 >= 60) {
+            situacao = "Aprovado"
+        }else if(media >= 50 || media < 60){
+            situacao = "Final"
+        }else{
+            situacao = "Reprovado"
+        }
+    console.log("Nome: " + student.name)
+    console.log("Média: " + media.toFixed(2)/4)
+    console.log("Situação: " + situacao)
+    }
+}
