a5df1327b029d436118f55af8616802b1e300cae
Tulio <tuuhlima@yahoo.com.br>
2017-10-25 20:55:19 -0200

    Finalizado

--- TulioLima/question-03/check-student.js
+++ TulioLima/question-03/check-student.js
@@ -7,16 +7,22 @@ function filter(id){
             var student = students[id-1];
             console.log(student.id);   
             console.log(student.name);
-            console.log(student.grades);
             for(var i = 0; i < 4;i++){
-                var soma = soma + student.grades[i];
-                console.log(""+student.grades[i]);    
+                var soma = soma + student.grades[i];   
             }
-            console.log("SOMA " + soma);                                
+            var media = soma/4;
+            console.log("Media " + media);
+            if(media >=60){
+                console.log("Situação: Aprovado");
+            }else{
+                if(media < 50){
+                    console.log("Situação: Reprovado");
+                }else{
+                    console.log("Situação: Final");
+                }
+            }                                
 }
 
+//Consegui resolver pelo indice você coloca o indice no NODEJS e ele ta da as informações.
 
-//for (var i = 0; i < students.length; i++){
- //               
-//            }
 
